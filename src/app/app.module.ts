01b03eec2d27514509bdb8d5245b778d5e5ba00b
Niko <nicoescudero@gmail.com>
2022-04-22 16:05:59 -0300

    arranque el projectcard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { AboutComponent } from './components/about/about.component';
+import { ListComponent } from './components/list/list.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProjectCardComponent } from './components/project-card/project-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AboutComponent
+    AboutComponent,
+    ListComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    LoginComponent,
+    ProjectCardComponent,
   ],
   imports: [
     BrowserModule,
