36cae477e8a9af94f022d954b689c94fc0f95016
Niko <nicoescudero@gmail.com>
2022-05-29 17:09:20 -0300

    experiencia service

---
+++ src/app/services/experiencia.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Experiencia } from '../models/Experiencia';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ExperienciaService {
+
+  url='https://agile-atoll-19905.herokuapp.com/api/experiencia';
+  constructor(private http:HttpClient) { }
+
+  getLista():Observable<any> {
+    return this.http.get(this.url);
+  }
+
+  getExperiencia(id: string):Observable<any> {
+    return this.http.get(this.url + "/" + id);
+  }
+
+  editExperiencia(id:string, experiencia: Experiencia):Observable<any> {
+    return this.http.put(this.url, experiencia);
+  }
+
+  addExperiencia(id:string, experiencia: Experiencia):Observable<any> {
+    return this.http.post(this.url, experiencia);
+  }
+
+  deleteExperiencia(id:string):Observable<any> {
+    return this.http.delete(this.url + "/" + id);
+  }
+
+}
