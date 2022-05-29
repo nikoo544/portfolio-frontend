416ac589429ae17a23522abf210ca8b6f549b895
Niko <nicoescudero@gmail.com>
2022-05-29 17:21:14 -0300

    proyecto service

---
+++ src/app/services/projects.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Proyecto } from '../models/Proyecto';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProjectsService {
+  
+    url='https://agile-atoll-19905.herokuapp.com/api/proyecto';
+  constructor(private http:HttpClient) { }
+  
+  getLista():Observable<any> {
+    return this.http.get(this.url);
+  }
+
+  getProyecto(id: string):Observable<any> {
+    return this.http.get(this.url + "/" + id);
+  }
+
+  editProyecto(id:string, proyecto: Proyecto):Observable<any> {
+    return this.http.put(this.url, proyecto);
+  }
+
+  addProyecto(id:string, proyecto: Proyecto):Observable<any> {
+    return this.http.post(this.url, proyecto);
+  }
+
+  deleteProyecto(id:string):Observable<any> {
+    return this.http.delete(this.url + "/" + id);
+  }
+
+}
